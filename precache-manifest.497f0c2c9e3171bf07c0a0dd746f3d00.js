095f5dc30a434eadc223afc82a02956b863d8e15
Madeleine Prak <madeleineprak@gmail.com>
2020-02-24 02:04:51 -0800

    Updates

---
+++ precache-manifest.497f0c2c9e3171bf07c0a0dd746f3d00.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aae58cf2d05f3cafec3f76bd6709899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59642560b8987d78644a",
+    "url": "/static/css/main.9b37c2e9.chunk.css"
+  },
+  {
+    "revision": "b1aafc9903e219a34537",
+    "url": "/static/js/2.9673bbde.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9673bbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59642560b8987d78644a",
+    "url": "/static/js/main.f94571f4.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "89f9a7311abd3d6297626c4a669f497e",
+    "url": "/static/media/FindMyHike.89f9a731.gif"
+  },
+  {
+    "revision": "6579825fdff5e53fa884a9b238e88f34",
+    "url": "/static/media/GetTheScoop.6579825f.gif"
+  },
+  {
+    "revision": "45fd3ffa329f81232c0adf65dbc6c1e5",
+    "url": "/static/media/HeyBud.45fd3ffa.gif"
+  },
+  {
+    "revision": "22b6af0a56bea91e58a852f5ecd4f2db",
+    "url": "/static/media/MemeHouse.22b6af0a.gif"
+  },
+  {
+    "revision": "c517b746a4560ed5b4f21307b3ef3b9f",
+    "url": "/static/media/MemoryGame.c517b746.gif"
+  },
+  {
+    "revision": "edb88740b9bf98d9e3b89d4e1345aa07",
+    "url": "/static/media/MomandPop.edb88740.gif"
+  },
+  {
+    "revision": "0a828b9a42d644502eeb0aa4ff98a0b9",
+    "url": "/static/media/Resume.0a828b9a.pdf"
+  },
+  {
+    "revision": "b819d940dbd89a1d3f0869538ff32312",
+    "url": "/static/media/arcgis.b819d940.png"
+  },
+  {
+    "revision": "9228f542564a699b423ed11b590a6254",
+    "url": "/static/media/htmlcssjs.9228f542.png"
+  },
+  {
+    "revision": "48824e775169f848f688df8a8f4f229b",
+    "url": "/static/media/ice-cream.48824e77.png"
+  },
+  {
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/static/media/jquery.a3af0655.png"
+  },
+  {
+    "revision": "537965147c3251b9f670e3dc739c7bc2",
+    "url": "/static/media/king.53796514.png"
+  },
+  {
+    "revision": "3c4539b33bdc56458e5be0d63d0bdc93",
+    "url": "/static/media/maps-and-location.3c4539b3.png"
+  },
+  {
+    "revision": "6f263c7c1b38c2b2fa2da8e5c849a0d0",
+    "url": "/static/media/me.6f263c7c.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/static/media/mongodb.e5eee315.png"
+  },
+  {
+    "revision": "44a6ff1a73ebe861bbd95c793a2598d5",
+    "url": "/static/media/mountain.44a6ff1a.png"
+  },
+  {
+    "revision": "46f758661588703b190fb4d2aae3847a",
+    "url": "/static/media/nodjs.46f75866.png"
+  },
+  {
+    "revision": "494d92ab3879b2418abbaff8fe52b895",
+    "url": "/static/media/photography.494d92ab.png"
+  },
+  {
+    "revision": "49eaa8acadf66605d5d36bc40353ffbd",
+    "url": "/static/media/profile-blue.49eaa8ac.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/static/media/react.df70b005.png"
+  },
+  {
+    "revision": "e63c867291d70480fd22f39d1e701863",
+    "url": "/static/media/sql.e63c8672.png"
+  },
+  {
+    "revision": "5e50767e001c3fba2e7b46c504584c57",
+    "url": "/static/media/tree.5e50767e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.5f4cc12f80774733b7eb74e91d7696c2.js"
+  "/precache-manifest.497f0c2c9e3171bf07c0a0dd746f3d00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
