4c1f1d94b85d4cf84db033a45de6819866a346ee
Madeleine Prak <madeleineprak@gmail.com>
2021-10-08 00:15:51 -0700

    update contact info

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
                                 <i className="fab fa-linkedin-in"></i>
                             </span>
                         </a>
-                        <a href="https://github.com/madeleineprak" target="_blank" rel="noopener noreferrer" className="button is-black is-outlined">
+                        <a href="https://github.com/passionfruition" target="_blank" rel="noopener noreferrer" className="button is-black is-outlined">
                             <span className="icon is-small">
                                 <i className="fab fa-github-alt"></i>
                             </span>
@@ -21,7 +21,7 @@ function Footer() {
                                 <i className="fab fa-twitter"></i>
                             </span>
                         </a>
-                        <a href="mailto:contact@madeleineprak.com" target="_blank" rel="noopener noreferrer" className="button is-black is-outlined">
+                        <a href="mailto:madeleineprak@gmail.com" target="_blank" rel="noopener noreferrer" className="button is-black is-outlined">
                             <span className="icon is-small">
                                 <i className="fas fa-envelope"></i>
                             </span>
